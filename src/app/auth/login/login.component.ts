b5ed36563bf1af934ccbbbf5944fa4e681d051c4
Kaviarasu <kavinskarasu@gmail.com>
2022-07-29 23:13:53 +0530

    updated

--- src/app/auth/login/login.component.ts
+++ src/app/auth/login/login.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { LoginForm } from 'src/app/types/auth';
-
+import { getAuth, signInWithEmailAndPassword } from 'firebase/auth';
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
@@ -16,5 +16,18 @@ export class LoginComponent implements OnInit {
   ngOnInit(): void {}
   email: string = '';
 
-  submit() {}
+  submit() {
+    const auth = getAuth();
+    signInWithEmailAndPassword(auth, this.form.email, this.form.password)
+      .then((userCredential) => {
+        // Signed in
+        alert('login successful');
+        // ...
+      })
+      .catch((error) => {
+        const errorCode = error.code;
+        const errorMessage = error.message;
+        alert('Credentials does not match our records');
+      });
+  }
 }
--- src/app/firebase.config.ts
+++ src/app/firebase.config.ts
@@ -1,16 +1,16 @@
 // Import the functions you need from the SDKs you need
-import { initializeApp } from "firebase/app";
+import { initializeApp } from 'firebase/app';
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
-const firebaseConfig = {
-  apiKey: "AIzaSyBgJ4x0znOs7RjfUzKsc437Nyz-VOPlnwk",
-  authDomain: "angular-833e2.firebaseapp.com",
-  projectId: "angular-833e2",
-  storageBucket: "angular-833e2.appspot.com",
-  messagingSenderId: "822673035572",
-  appId: "1:822673035572:web:1421a049916d422cead561",
+export const firebaseConfig = {
+  apiKey: 'AIzaSyBgJ4x0znOs7RjfUzKsc437Nyz-VOPlnwk',
+  authDomain: 'angular-833e2.firebaseapp.com',
+  projectId: 'angular-833e2',
+  storageBucket: 'angular-833e2.appspot.com',
+  messagingSenderId: '822673035572',
+  appId: '1:822673035572:web:1421a049916d422cead561',
 };
 
 // Initialize Firebase
